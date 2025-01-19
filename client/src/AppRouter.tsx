7a9d3c95fb6f0c2023abeb5815ba3d97804b810b
dragos <dragosdm22@gmail.com>
2025-01-19 19:01:34 +0200

    add protection

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -1,36 +1,72 @@
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import LoginPage from "./pages/login/loginPage";  // Assuming you have this page component
-import AddNew from "./pages/customer/addNew";
-import HomePage from "./pages/home/homePage";
-import AddNewUserPage from "./pages/user/addNew";
-import ProfilePage from "./pages/profile/profilePage";
-import SettingsPage from "./pages/settings/settingsPage";
-import ReportsPage from "./pages/reports/reportsPage";
-import UpcomingPage from "./pages/inspections/upcoming/upcomingPage";
-import DailyPage from "./pages/inspections/daily/dailyPage";
-import MonthlyPage from "./pages/inspections/monthly/monthlyPage";
-import CustomersListPage from "./pages/customer/customersList";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LoginPage from './pages/login/loginPage';
+import AddNew from './pages/customer/addNew';
+import HomePage from './pages/home/homePage';
+import AddNewUserPage from './pages/user/addNew';
+import ProfilePage from './pages/profile/profilePage';
+import SettingsPage from './pages/settings/settingsPage';
+import ReportsPage from './pages/reports/reportsPage';
+import UpcomingPage from './pages/inspections/upcoming/upcomingPage';
+import DailyPage from './pages/inspections/daily/dailyPage';
+import MonthlyPage from './pages/inspections/monthly/monthlyPage';
+import CustomersListPage from './pages/customer/customersList';
+import { AuthProvider } from './contexts/authContext'; // Wrap with AuthProvider
+import ProtectedRoute from './PrivateRoute';
 
 const AppRouter = () => {
-    return (
-        <Router>
-            <div>
-                <Routes>
-                    <Route path="customer/add-new" element={<AddNew />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/home" element={<HomePage />} />
-                    <Route path="/users/add-new" element={<AddNewUserPage />} />
-                    <Route path="/profile" element={<ProfilePage />} />
-                    <Route path="/settings" element={<SettingsPage />} />
-                    <Route path="/reports" element={<ReportsPage />} />
-                    <Route path="inspections/upcoming" element={<UpcomingPage />} />
-                    <Route path="inspections/daily" element={<DailyPage />} />
-                    <Route path="inspections/monthly" element={<MonthlyPage />} />
-                    <Route path="customer/list" element={<CustomersListPage />} />
-                </Routes>
-            </div>
-        </Router>
-    );
-}
+  return (
+    <AuthProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            
+            {/* Public Routes */}
+            <Route path="/home" element={<HomePage />} />
+
+
+            {/* Protected Routes */}
+            <Route 
+              path="customer/list"
+              element={<ProtectedRoute element={<CustomersListPage />}/>}
+            />
+            <Route
+              path="customer/add-new"
+              element={<ProtectedRoute element={<AddNew />} />}
+            />
+            <Route
+              path="/users/add-new"
+              element={<ProtectedRoute element={<AddNewUserPage />} />}
+            />
+            <Route
+              path="/profile"
+              element={<ProtectedRoute element={<ProfilePage />} />}
+            />
+            <Route
+              path="/settings"
+              element={<ProtectedRoute element={<SettingsPage />} />}
+            />
+            <Route
+              path="/reports"
+              element={<ProtectedRoute element={<ReportsPage />} />}
+            />
+            <Route
+              path="inspections/upcoming"
+              element={<ProtectedRoute element={<UpcomingPage />} />}
+            />
+            <Route
+              path="inspections/daily"
+              element={<ProtectedRoute element={<DailyPage />} />}
+            />
+            <Route
+              path="inspections/monthly"
+              element={<ProtectedRoute element={<MonthlyPage />} />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+};
 
 export default AppRouter;
